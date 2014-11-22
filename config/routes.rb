8201de2555924700e588c6b04b6b18aa102f27d7
Keisuke Futonaka <futonaka@gmail.com>
2014-11-22 22:27:11 +0900

    Add routes for facebook callback.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "home", to: "home#index", as: "user_root"
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'home/index'
 
   get 'home/show'
