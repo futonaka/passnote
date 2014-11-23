eba5564a93979120f836007dc52afdc4e3fe08a6
Keisuke Futonaka <futonaka@gmail.com>
2014-11-23 13:20:22 +0900

    Modify text and styles.

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -42,4 +42,18 @@ SimpleForm.setup do |config|
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
   config.default_wrapper = :bootstrap
+
+  config.wrappers :checkbox, tag: :div, class: "checkbox", error_class: "has-error" do |b|
+    # Form extensions
+    b.use :html5
+
+    # Form components
+    b.wrapper tag: :label do |ba|
+      ba.use :input
+      ba.use :label_text
+    end
+
+    b.use :hint,  wrap_with: { tag: :p, class: "help-block" }
+    b.use :error, wrap_with: { tag: :span, class: "help-block text-danger" }
+  end
 end
