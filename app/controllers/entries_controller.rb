a89fb3e8827c6efa16de01e8fe44485cfa1f70b2
Keisuke Futonaka <futonaka@gmail.com>
2014-11-25 00:58:01 +0900

    Add user check for entries.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,12 @@
 class EntriesController < ApplicationController
   before_action :set_entry, only: [:show, :edit, :update, :destroy]
+  before_action :validate_user!
 
   respond_to :html
 
-#  def index
-#    @entries = Entry.all
-#    respond_with(@entries)
-#  end
+  def index
+    redirect_to notes_path
+  end
 
   def show
     respond_with(@entry)
@@ -48,5 +48,11 @@ class EntriesController < ApplicationController
 
     def entry_params
       params.require(:entry).permit(:note_id, :author_id, :weather, :date, :body)
+    end
+
+    def validate_user!
+      if @entry && !@entry.note.users.include?(current_user)
+        redirect_to notes_path, alert: "参照権限の無い日記です"
+      end
     end
 end
